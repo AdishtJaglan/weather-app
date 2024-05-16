ddca1a0cb0f480a93f0ad407b77269cb3b95e586
AdishtJaglan <adisht7524@gmail.com>
2024-05-16 21:57:47 +0530

    setup data retrieval from api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import "./style.css";
+import { getCityName } from "./weatherData";
 
 console.log("connected");
+getCityName();
---
+++ src/weatherData.js
@@ -0,0 +1,22 @@
+export async function getWeather(city) {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=cb043f74de124798b5073115241605&q=${city}`
+  );
+  const data = await response.json();
+  const feelsLike = data.current.condition.text;
+  const location = data.location.name;
+  const weatherIcon = data.current.condition.icon;
+  const tempreture = data.current.temp_c;
+  const tempretureFeelsLike = data.current.feelslike_c;
+}
+
+export function getCityName() {
+  const cityName = document.querySelector("#city");
+  const cityForm = document.querySelector("form");
+
+  cityForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    getWeather(cityName.value);
+  });
+}
